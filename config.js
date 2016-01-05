bb3011993ef38f2c9b722c36ea8d87a1f2ddc1aa
NaOH <nstbayless@gmail.com>
2016-01-05 15:49:35 -0800

    Now interfaces correctly with libgyoa.so

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {}
+
+//path to adventure data
+config.model_path = "~/git/gyoa-adv"
+
+//export module (for use in server.js)
+module.exports = config
--- server.js
+++ server.js
@@ -3,15 +3,31 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path")
 var url = require("url");
+var ffi = require("ffi");
+var ref = require('ref');
+var Struct = require('ref-struct');
+var sanitizeHtml = require('sanitize-html');
+
+//load config options:
+var config = require("./config")
 
 //----GYOA----//
-
-//ffi import
-
-var ffi = require("ffi");
-var gyoa = ffi.Library('libgyoa',{
-  'getBuildDate' : ['string', [ ] ]
+var GyoaID = Struct({
+  'gid': 'int',
+  'rid': 'int'
 })
+
+var gyoa = ffi.Library('libgyoa',{
+  'getBuildDate' : ['string', [ ] ],
+  'loadModel' : ['pointer', ['string'] ],
+  'getRoomTitle' : ['string', ['pointer',GyoaID] ],
+  'getRoomBody' : ['string', ['pointer',GyoaID] ],
+  'parse_id' : [GyoaID, ['string'] ],
+})
+
+//load GYOA library:
+var gyoa_model = gyoa.loadModel(config.model_path)
+var gyoa_inittag = "0x0"
 
 //suggested from here http://stackoverflow.com/q/7268033
 var mimeTypes = {
@@ -24,13 +40,37 @@ var mimeTypes = {
 var embed_top = fs.readFileSync('private/embed_top.html').toString();
 var embed_bot = fs.readFileSync('private/embed_bot.html').toString();
 
+var getHTMLBodyText = function(raw_text) {
+  if (raw_text.substring(0,5).toLowerCase()=="@html") {
+    //text is natively HTML. Just remove @html token and sanitize
+    return sanitizeHtml(raw_text.substring(5), {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
+    });
+  } else {
+    //text is not HTML. reformat:
+    var paragraphs = raw_text.split("\n\n")
+    var html_text = ""
+    var first=true;
+    for (var i in paragraphs){
+      if (!first)
+        html_text+="<br/><br/>"
+      html_text+=paragraphs[i];
+      first=false;
+    }
+    return html_text;
+  }
+}
+
 var getScenario = function(tag) {
   var tags=tag.substring(1).split("x")
   if (tags.length>=2){
     gid=tags[0]
     rid=tags[1]
-    var header="Header"
-    var body = "Body text for room #"+gid
+    var gyoa_rm_id = gyoa.parse_id(gid+":"+rid);
+
+    //get room description:
+    var title = gyoa.getRoomTitle(gyoa_model,gyoa_rm_id)
+    var body = getHTMLBodyText(gyoa.getRoomBody(gyoa_model,gyoa_rm_id))
     
     var opt = [ ]
     for (var i=0;i<3;i++){
@@ -40,7 +80,7 @@ var getScenario = function(tag) {
       })
     }
     return {
-      header: header,
+      title: title,
       body: body,
       opt: opt
     }
@@ -53,7 +93,7 @@ var makeResponseForTag = function(tag,response) {
   response.write(embed_top);
   var room = getScenario(tag)
   if (room!=null) {
-    response.write("<tr><th>"+room.header+"</th></tr>\n")
+    response.write("<tr><th>"+room.title+"</th></tr>\n")
     response.write("<tr><td>"+room.body+"</td></tr>\n")
     if (room.opt.length>0) {
       response.write("<tr><td>")
@@ -63,6 +103,9 @@ var makeResponseForTag = function(tag,response) {
       }
       response.write("</td></tr>")
     }
+  } else { //no scenario found
+    response.write("<tr><td> Error in URL tags (" + tag + ").<br/> No scenario found.</tr></td>\n"
+                   + "<tr><td><a href=" + gyoa_inittag +">restart from scratch?</a></td></tr>\n")
   }
   response.write(embed_bot);
   response.end();
