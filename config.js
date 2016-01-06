e24117f11afd6b305228ad38fb337ff58ede7e27
NaOH <nstbayless@gmail.com>
2016-01-05 17:06:42 -0800

    Can now load options from room.

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 var config = {}
 
 //path to adventure data
-config.model_path = "~/git/gyoa-adv"
+config.model_path = "~/git/gyoa-test-repo"
+
+//port to listen on
+config.port=8888
+
+//allow users to edit adventure?
+config.editable=false;
 
 //export module (for use in server.js)
 module.exports = config
--- server.js
+++ server.js
@@ -20,8 +20,12 @@ var GyoaID = Struct({
 var gyoa = ffi.Library('libgyoa',{
   'getBuildDate' : ['string', [ ] ],
   'loadModel' : ['pointer', ['string'] ],
+  'roomExists' : ['bool', ['pointer',GyoaID] ],
   'getRoomTitle' : ['string', ['pointer',GyoaID] ],
   'getRoomBody' : ['string', ['pointer',GyoaID] ],
+  'getOptionCount' : ['int', ['pointer',GyoaID] ],
+  'getOptionDescription' : ['string', ['pointer',GyoaID,'int'] ],
+  'getOptionDestination' : [GyoaID, ['pointer',GyoaID,'int'] ],
   'parse_id' : [GyoaID, ['string'] ],
 })
 
@@ -40,6 +44,7 @@ var mimeTypes = {
 var embed_top = fs.readFileSync('private/embed_top.html').toString();
 var embed_bot = fs.readFileSync('private/embed_bot.html').toString();
 
+//get HTML representation of the given body text
 var getHTMLBodyText = function(raw_text) {
   if (raw_text.substring(0,5).toLowerCase()=="@html") {
     //text is natively HTML. Just remove @html token and sanitize
@@ -48,35 +53,44 @@ var getHTMLBodyText = function(raw_text) {
     });
   } else {
     //text is not HTML. reformat:
+    if (raw_text.substring(0,6)=="@exact") {
+      //display exactly as written:
+      return "<pre>"+raw_text.substring(6)+"</pre>"
+    }
+    //convert to paragraph form:
     var paragraphs = raw_text.split("\n\n")
     var html_text = ""
-    var first=true;
     for (var i in paragraphs){
-      if (!first)
-        html_text+="<br/><br/>"
-      html_text+=paragraphs[i];
-      first=false;
+      html_text+="<p>"+paragraphs[i]+"</p>\n";
     }
+    html_text+=""
     return html_text;
   }
 }
 
+//retrieve room struct for given tag (e.g. room 0x0)
 var getScenario = function(tag) {
-  var tags=tag.substring(1).split("x")
+  var tags=tag.split("x")
   if (tags.length>=2){
     gid=tags[0]
     rid=tags[1]
     var gyoa_rm_id = gyoa.parse_id(gid+":"+rid);
-
+    if (!gyoa.roomExists(gyoa_model,gyoa_rm_id))
+      return null;
     //get room description:
     var title = gyoa.getRoomTitle(gyoa_model,gyoa_rm_id)
     var body = getHTMLBodyText(gyoa.getRoomBody(gyoa_model,gyoa_rm_id))
     
     var opt = [ ]
-    for (var i=0;i<3;i++){
+    var opt_n = gyoa.getOptionCount(gyoa_model,gyoa_rm_id);
+    for (var i=0;i<opt_n;i++){
+      var opt_text = gyoa.getOptionDescription(gyoa_model,gyoa_rm_id,i);
+      var dst_id = gyoa.getOptionDestination(gyoa_model,gyoa_rm_id,i);
+      var opt_href = dst_id.gid + "x" + dst_id.rid;
+      console.log(opt_href + ":" + opt_text);
       opt.push({
-        description: ("go to room "+i),
-        destination: (i+"x0")
+        description: opt_text,
+        destination: opt_href
       })
     }
     return {
@@ -88,6 +102,7 @@ var getScenario = function(tag) {
   return null;
 }
 
+//send client HTML corresponding to the desired gyoa room and context
 var makeResponseForTag = function(tag,response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(embed_top);
@@ -99,7 +114,14 @@ var makeResponseForTag = function(tag,response) {
       response.write("<tr><td>")
       response.write("<p align=\"center\" size=24>Options</p>\n")
       for (var i=0;i<room.opt.length;i++) {
-       response.write("<p><a href="+room.opt[i].destination+">"+room.opt[i].description+"</a></p>\n")
+       if (room.opt[i].destination.substring(0,5)=="-1x-1") {
+         //no destination written
+         if (config.editable)
+           response.write("<p><a href="+room.opt[i].destination+">"+room.opt[i].description+"</a>*</p>\n")
+         else
+           response.write("<p>"+room.opt[i].description+"</p>")
+       } else
+         response.write("<p><a href="+room.opt[i].destination+">"+room.opt[i].description+"</a></p>\n")
       }
       response.write("</td></tr>")
     }
@@ -111,6 +133,7 @@ var makeResponseForTag = function(tag,response) {
   response.end();
 }
 
+//send client the given file (in the public/ folder)
 var makeResponseForFile = function(file,res) {
   //console.log("file requested: " + file)
   var uri = url.parse("public" +file).pathname;
@@ -122,18 +145,19 @@ var makeResponseForFile = function(file,res) {
     fileStream.pipe(res);
   } else {
     console.log("not exists: " + uri);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(404, {'Content-Type': 'text/plain'});
     res.write('404 Not Found\n');
     res.end();
   }
 }
 
+//start serving...
 http.createServer(function(request, response) {
   if (request.url.substring(0,5)=="/pub/") {
     makeResponseForFile(request.url.substring(4),response);
   } else {
-    makeResponseForTag(request.url,response);
+    makeResponseForTag(request.url.substring(1),response);
   }
-}).listen(8888);
+}).listen(config.port);
 
-console.log("Server running on port 8888");
+console.log("Server running on port "+config.port);
